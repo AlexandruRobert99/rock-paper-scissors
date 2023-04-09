e8fbfafc9d41bfbbc036fb7517049feae88c0999
Alex <83609494+AlexandruRobert99@users.noreply.github.com>
2023-04-09 20:25:05 +0300

    Creating a new function that decides who is the winner between the player and computer.

--- script.js
+++ script.js
@@ -1,18 +1,63 @@
-function getComputerChoice() {
+let computerSelection = null;
+
+function getComputerSelection() {
     const randomNumber = Math.floor(Math.random() * 3);
     switch(randomNumber) {
         case 0:
-            console.log("rock");
+            computerSelection = "rock";
             break;
         case 1:
-            console.log("paper");
+            computerSelection = "paper";
             break;
         case 2:
-            console.log("scissors");
+            computerSelection = "scissors";
             break;
-        default:
-            console.log("Something went wrong.")
     }
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case "rock":
+            if (computerSelection === "rock") {
+                return("Tie");
+            } else if (computerSelection === "paper"){
+                return("You Lose! Paper beats Rock");
+            } else {
+                return("You Win! Rock beats Scissors");
+            }
+            break;
+
+        case "paper":
+            if (computerSelection === "rock") {
+                return("You Win! Paper beats Rock");
+            } else if (computerSelection === "paper"){
+                return("Tie");
+            } else {
+                return("You Lose! Scissors beats Paper");
+            }
+            break;
+
+        case "scissors":
+            if (computerSelection === "rock") {
+                return("You Lose! Rock beats Scissors");
+            } else if (computerSelection === "paper"){
+                return("You Win! Scissors beats Paper");
+            } else {
+                return("Tie");
+            }
+            break;
+        default:
+            return("You didn't select between rock, paper or scissors.")
+    }
+}
+
+
+const playerSelection = prompt("Choose between rock, paper or scissors: ").toLowerCase();
+
+getComputerSelection(); // The computer gets a random value between "rock", "paper" or "scissors"
+
+console.log("Player selection: " + playerSelection);
+
+console.log("Computer selection: " + computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
